c20c4bee2ed51411c37f5ba6a9a42198214158b7
William-Tygret <wctygret@gmail.com>
2016-02-18 13:01:08 -0600

    fragments

---
+++ starter-code/UserInfoTabs/app/src/main/java/ly/generalassemb/drewmahrt/userinfotabs/PersonalInfoFragment.java
@@ -0,0 +1,20 @@
+package ly.generalassemb.drewmahrt.userinfotabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by williamtygret on 2/18/16.
+ */
+public class PersonalInfoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.personal_info_frag,container,false);
+    }
+}
